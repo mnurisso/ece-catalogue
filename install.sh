a41473f8356077b1f5c536439de4445f02f0e114
Jost von Hardenberg <hardenberg@gmail.com>
2025-03-06 17:16:11 +0000

    grids from swift

--- install.sh
+++ install.sh
@@ -14,7 +14,13 @@ ln -s $SCRIPT_DIR/config/grids/EC-EARTH4.yaml $AQUA_DIR/grids/
 aqua add hpc2020 -e $SCRIPT_DIR/catalogs/hpc2020
 
 dst=$(grep -A 2 '^hpc2020:' $SCRIPT_DIR/catalogs/hpc2020/machine.yaml|tail -1| sed -E 's/.*grids:\s*(.*)/\1/')
-mkdir -p $dst/EC-EARTH4bis
-cp $SCRIPT_DIR/grids/*.nc $dst/EC-EARTH4bis/
+
+mkdir -p $dst/EC-EARTH4
+cd $dst
+
+# Download grids from DKRZ Swift
+wget "https://swift.dkrz.de/v1/dkrz_a973e394-5f24-4f4d-8bbf-1a83bd387ccb/AQUA/grids/EC-EARTH4.tar.gz?temp_url_sig=46efc8c2277242713a7999f8b6fb2d49fb340b91&temp_url_expires=2027-02-04T17:04:56Z" -O EC-EARTH4.tar.gz
+tar xvfz EC-EARTH4.tar.gz
+rm EC-EARTH4.tar.gz
 
 echo "Installation complete"
